4a17d538ebccecd35e8147c19b3379e2c381e0cf
fannilin <103945012+fannilin@users.noreply.github.com>
2023-01-26 22:01:08 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [item, setItem] = useState('');
+  const [lista, setLista] = useState([]);
+
+  const addlista = () => {
+    setLista([item, ...lista]);
+    setItem('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ostoslista</Text>
+      <TextInput
+      value={item} 
+      style={{width: 200, borderColor: 'orange', borderWidth: 1}}
+      onChangeText= {item => setItem(item)}
+      >
+      </TextInput> 
+      <Button title= 'Lisää listaan' onPress={addlista} ></Button>
+      <FlatList 
+      data={lista}
+      renderItem={({item}) => <Text>{item}</Text>}></FlatList>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,6 +32,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 80,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
